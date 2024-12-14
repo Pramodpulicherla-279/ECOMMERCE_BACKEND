11ecf07eaf228d03dfb7ce4322456a98ebbbd90a
Pramodpulicherla-279 <pramodpulicherla279@gmail.com>
2024-12-14 23:42:29 +0530

    major changes

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -26,32 +25,3 @@ def read_root():
 
 # Include your routers here
 app.include_router(upload_router)
-=======
-# main.py
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from upload import router as upload_router
-
-
-app = FastAPI(docs_url="/docs")
-
-origins = [
-    "*",  # Allow all origins
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-# Include your routers here
-app.include_router(upload_router)
->>>>>>> 376bc0522af707335cef0800c1ef2e4f14f41be5
--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from sqlalchemy import Column, Integer, String, Float, JSON, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -24,30 +23,4 @@ class Product(Base):
     mainImageUrl = Column(String)
     demanded = Column(Boolean)  # Add the demanded column
 
-=======
-from sqlalchemy import Column, Integer, String, Float, JSON
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-import urllib.parse
-
-Base = declarative_base()
-
-# URL-encode the password
-password = urllib.parse.quote_plus('Pramod@23057')
-engine = create_engine(f'mysql+mysqlconnector://root:{password}@127.0.0.1/Ecommerce_DB')
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-class Product(Base):
-    __tablename__ = "products"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    description = Column(String)
-    price = Column(Float)
-    stock = Column(Integer)
-    category = Column(String)
-    imageUrls = Column(JSON)  # Use JSON column to store list of URLs
-    mainImageUrl = Column(String)
-
->>>>>>> 376bc0522af707335cef0800c1ef2e4f14f41be5
 Base.metadata.create_all(bind=engine)
